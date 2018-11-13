c6dbbe51c6d0bd23f8ce77dd424e89656b72b0aa
Ignaciopetru <40152963+Ignaciopetru@users.noreply.github.com>
2018-11-13 17:02:05 -0300

    Update PruebaTest.php

--- tests/PruebaTest.php
+++ tests/PruebaTest.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\TestCase;
 class testPrueba extends TestCase {
 
     public function testPruebas() {
-        $prueba = New Prueba( "./tests/archivoTest.yml", 1, "Matematica");
+        $prueba = New Prueba( "./tests/archivoTest.yml", 1, "Matematica", "23/5/12");
         $prueba->crearPreguntas();
         $this->assertEquals(count($prueba->devuelvePreguntas()), 1);
     }
