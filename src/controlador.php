a8fc936165f71fd048c299db9bb033c4d2cbc234
Ignaciopetru <40152963+Ignaciopetru@users.noreply.github.com>
2018-11-25 17:59:45 -0300

    Update controlador.php

--- src/controlador.php
+++ src/controlador.php
@@ -8,9 +8,10 @@ $canTemas = $_POST['canTemas'];
 $archivo = $_POST['archivo'];
 $materia = $_POST['materia'];
 $fecha = $_POST['fecha'];
+$cantPreg = $_POST['cantPreg'];
 
 for ($i=1; $i <= $canTemas; $i++) {
-    $salida = new Prueba($archivo, $i, $materia, $fecha);
+    $salida = new Prueba($archivo, $i, $materia, $fecha, $cantPreg);
     $salida->crearPreguntas();
     $salida->mezclarPreguntas();
     $salida->crearHTML();
